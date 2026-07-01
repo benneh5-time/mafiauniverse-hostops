fe4ac258faf1d6e5c0820189651be55439686d54
ben <nebbeh@gmail.com>
2026-07-01 14:58:41 -0700

    Pet mafia and town

--- host_ops/commands/rolepm.py
+++ host_ops/commands/rolepm.py
@@ -34,6 +34,18 @@ Ability that's very cool
 [*][B]Very Mid Ability[/B] — [I]tags[/I]
 Ability 2 that's mid[/LIST][/QUOTE]"""
 
+_PET_BLOCK = """
+
+[BOX=Pet Name— Pet][CENTER][CHARIMG]picture here[/CHARIMG][/CENTER]
+
+[B][U]While you have your pet, you also have the following abilities:[/U][/B]
+[LIST=1][*][B]ability name[/B]
+Ability here [/LIST]
+[/BOX]"""
+
+_TOWN_PET_TEMPLATE = _TOWN_TEMPLATE[: -len("[/QUOTE]")] + _PET_BLOCK + "[/QUOTE]"
+_MAFIA_PET_TEMPLATE = _MAFIA_TEMPLATE[: -len("[/QUOTE]")] + _PET_BLOCK + "[/QUOTE]"
+
 
 def register(bot, *, mu_user_ids: dict[str, tuple[str, int]]) -> None:
     async def _rolepm(ctx, name: str, template: str) -> None:
@@ -63,3 +75,19 @@ def register(bot, *, mu_user_ids: dict[str, tuple[str, int]]) -> None:
     @bot.command(name="mafia")
     async def mafia(ctx, *, name: str):
         await _rolepm(ctx, name, _MAFIA_TEMPLATE)
+
+    @bot.command(name="pettown")
+    async def pettown(ctx, *, name: str):
+        await _rolepm(ctx, name, _TOWN_PET_TEMPLATE)
+
+    @bot.command(name="petmafia")
+    async def petmafia(ctx, *, name: str):
+        await _rolepm(ctx, name, _MAFIA_PET_TEMPLATE)
+
+    @bot.command(name="petvillager")
+    async def petvillager(ctx, *, name: str):
+        await _rolepm(ctx, name, _TOWN_PET_TEMPLATE)
+
+    @bot.command(name="petwolf")
+    async def petwolf(ctx, *, name: str):
+        await _rolepm(ctx, name, _MAFIA_PET_TEMPLATE)
